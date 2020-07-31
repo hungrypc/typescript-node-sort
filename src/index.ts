04d4a1b11b43c69e3d8ffdcff814538fb2135c95
hpc <philipchanhk626@gmail.com>
2020-07-31 10:43:42 -0400

    implemented CharactersCollection, sorting works as expected

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
+import { CharactersCollection } from './CharactersCollection' 
 
 const numbersCollection = new NumbersCollection([10, 2, -3, 0])
+const charactersCollection = new CharactersCollection('asdfgh')
 const sorter = new Sorter(numbersCollection)
+const sorter2 = new Sorter(charactersCollection)
 sorter.sort()
-console.log(sorter.collection.data)
+sorter2.sort()
+console.log(sorter.collection)
+console.log(sorter2.collection)
